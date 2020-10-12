5f5d7cec24fed00d23f79a95162e11a45ca82630
John Evans <jevans667cc@gmail.com>
2020-10-12 10:53:10 -0400

    Test tmp file

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -4,6 +4,11 @@ file { '/usr/local/bin/run-puppet':
   mode   => '0755',
 }
 
+file { '/tmp/hello.txt':
+  content => 'hi there',
+  mode   => '0755',
+}
+
 cron { 'run-puppet':
   command => '/usr/local/bin/run-puppet',
   hour    => '*',
